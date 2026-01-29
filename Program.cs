93040245d497fee6a7d09e2b3606a355bbdc032a
Mirko 🇪🇺 🇺🇦 <4560672+mu88@users.noreply.github.com>
2026-01-29 14:07:21 +0100

    feat: initialize repo

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+       .AddOpenTelemetry()
+       .UseOtlpExporter()
+       .ConfigureResource(c => c.AddService("weather-forecast-service"))
+       .WithMetrics(metrics =>
+       {
+           metrics
+               .AddAspNetCoreInstrumentation()
+               .AddProcessInstrumentation()
+               .AddRuntimeInstrumentation();
+       });
+
+var app = builder.Build();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", (ILogger<Program> logger) =>
+{
+    logger.LogInformation("Generating weather forecast");
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+});
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary);
